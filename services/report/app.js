2e707fe67e3cdf9793931675710d67ff27936747
clapscofield <clarisse.scofield@gmail.com>
2022-01-21 16:58:14 -0300

    Criação do serviço Report

--- services/report/app.js
+++ services/report/app.js
@@ -4,6 +4,11 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 var report = {}
+
+function hasOrders(deliveryData) {
+    return Array.isArray(deliveryData.products) && deliveryData.products.length > 0
+}
+
 async function updateReport(products) {
     for(let product of products) {
         if(!product.name) {
@@ -17,6 +22,21 @@ async function updateReport(products) {
 
 }
 
+async function processMessage(msg) {
+    const deliveryData = JSON.parse(msg.content)
+    try {
+        if (hasProducts(deliveryData)) {
+            updateReport(deliveryData.products)
+            printReport()
+            console.log(`✔ RELATÓRIO GERADO`)
+        } else {
+            console.log(`X NÃO É POSSÍVEL GERAR RELATÓRIO DE PEDIDO SEM PRODUTOS`)
+        }
+    } catch (error) {
+        console.log(`X ERROR TO PROCESS: ${error.response}`)
+    }
+}
+
 async function printReport() {
     for (const [key, value] of Object.entries(report)) {
         console.log(`${key} = ${value} vendas`);
@@ -24,7 +44,8 @@ async function printReport() {
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
-} 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)})
+}
 
 consume()
